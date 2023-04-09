9dc5bbe74abcbd848f74a099bf5baffdce54a96b
DESKTOP-08L14TP\매장PC <suni980215@gamil.com>
2023-04-09 21:13:46 +0900

    초기상태

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-function Home() {
-    const [item,setItem] = useState(0);
-
-    const plusBtn = () => {
-        setItem(item + 1);
-    };
-    const miusBtn = () => {
-        setItem(item - 1);
-    };
-
+function home() {
   return (
-    <div>
-        <p>{item}</p>
-        <button onClick={plusBtn}>+</button>
-        <button onClick={miusBtn}>-</button>
-    </div>
+    <div>home</div>
   )
 }
 
-export default Home
+export default home
