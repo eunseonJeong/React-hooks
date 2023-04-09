ddacaa91d38bc903e7f2a67084bca10fc5780f0b
DESKTOP-08L14TP\매장PC <suni980215@gamil.com>
2023-04-09 21:07:13 +0900

    update : react-hooks

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+function Home() {
+    const [item,setItem] = useState(0);
+
+    const plusBtn = () => {
+        setItem(item + 1);
+    };
+    const miusBtn = () => {
+        setItem(item - 1);
+    };
+
+  return (
+    <div>
+        <p>{item}</p>
+        <button onClick={plusBtn}>+</button>
+        <button onClick={miusBtn}>-</button>
+    </div>
+  )
+}
+
+export default Home
